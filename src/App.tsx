5c5ac62da93e1a309e7875bf432d70d041eaf5af
Andre Lipiec <andrelipiec@gmail.com>
2023-05-30 13:56:08 -0400

    minor input style change

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
       <h2 className="text-4xl font-bold pb-6">Todo List</h2>
       <form onSubmit={handleFormSubmit}>
         <label className="block mb-2 text-sm font-medium text-gray-900">Input</label>
-        <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-4" placeholder="Todo item" required value={input} onChange={handleInputChange}/>
+        <input type="text" id="first_name" className="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-4" placeholder="Todo item" required value={input} onChange={handleInputChange}/>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Add Todo</button>
       </form>
       <div className="mt-10">
